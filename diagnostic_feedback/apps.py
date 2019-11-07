7b9144136a6ba6f6aa0f24fb8f5da8342f7819da
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2019-11-07 20:13:31 +0500

    Changes related to ironwood rebase

---
+++ diagnostic_feedback/apps.py
@@ -0,0 +1,15 @@
+"""
+Configuration for course_overviews Django app
+"""
+from django.apps import AppConfig
+
+
+class DiagnosticFeedbackConfig(AppConfig):
+    """
+    Configuration class for course_overviews Django app
+    """
+    name = 'diagnostic_feedback'
+    verbose_name = "Diagnostic Feedback"
+
+    def ready(self):
+        from .quiz import QuizBlock
